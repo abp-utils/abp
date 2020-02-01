14e86a718be8aeb31fb48d1a2adfb13c2f262ef9
KreeK <kirill15071999@yandex.ru>
2020-02-01 17:00:41 +0300

    Autoload fix

--- core/Autoload.php
+++ core/Autoload.php
@@ -3,12 +3,10 @@
 function autoload($classname)
 {
     $classname = str_replace( '\\', '/', $classname);
-    echo $classname . '<br>';
     $tempClassname = explode('/', $classname);
     if ($tempClassname[0] == Abp::NAME) {
         unset($tempClassname[0]);
         $tempClassname = implode('/', $tempClassname);
-        echo $tempClassname . ' 1<br>';
         if (file_exists(__DIR__. "/../$tempClassname.php")) {
             include_once __DIR__. "/../$tempClassname.php";
             return;
@@ -17,7 +15,6 @@ function autoload($classname)
 
     if (file_exists( "{$_SERVER['DOCUMENT_ROOT']}/$classname.php")) {
         include_once "{$_SERVER['DOCUMENT_ROOT']}/$classname.php";
-    } else
-        echo $classname . '<br>';
+    }
 }
 spl_autoload_register('autoload');
