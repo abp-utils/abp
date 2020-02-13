af384dd01f6bf8bd7b352259b3b5f0f394471d0e
KreeK <kirillnovikovofficial@gmail.com>
2020-02-13 17:45:49 +0300

    Model: new methods

--- core/Model.php
+++ core/Model.php
@@ -5,19 +5,33 @@ namespace abp\core;
 use abp\component\StringHelper;
 use Abp;
 
+/**
+ * Class Model
+ * @package abp\core
+ */
 class Model
 {
     protected $_attributes = [];
     protected $_changeAttributes = [];
+    protected $_unsetAttributes = [];
 
     protected $_tableName = null;
 
+    /**
+     * Model constructor.
+     * @param string $class
+     * @param array $params
+     */
     public function __construct($class, $params = [])
     {
         $this->_tableName = StringHelper::conversionFilename($class);
         $this->_attributes = $params;
     }
 
+    /**
+     * @param string $name
+     * @return mixed
+     */
     public function __get($name)
     {
         if (!isset($this->_attributes[$name])) {
@@ -26,6 +40,10 @@ class Model
         return $this->_attributes[$name];
     }
 
+    /**
+     * @param string $name
+     * @param string $value
+     */
     public function __set($name, $value)
     {
         if (!isset($this->_attributes[$name])) {
@@ -35,11 +53,63 @@ class Model
         $this->_changeAttributes[$name] = $value;
     }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        if (!isset($this->_attributes[$name])) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function __unset($name)
+    {
+        if (!isset($this->_attributes[$name])) {
+            throw new \InvalidArgumentException("Свойство $name не существует в модели " . self::class);
+        }
+        $this->_unsetAttributes[$name] = $value;
+        unset($this->_attributes[$name]);
+    }
+
+    /**
+     * @return array
+     */
     public function __debugInfo()
     {
         return [
             '_attributes:protected' => $this->_attributes,
             '_changeAttributes:protected' => $this->_changeAttributes,
+            '_unsetAttributes:protected' => $this->_unsetAttributes,
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->_attributes;
+    }
+
+    /**
+     * @return array
+     */
+    public function getChangeAttributes()
+    {
+        return $this->_changeAttributes;
+    }
+
+    /**
+     * 
+     */
+    public function getUnsetAttributes()
+    {
+        $this->_unsetAttributes;
     }
 }
